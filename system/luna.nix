a8205c938fb7c156e949ff7e58fb0160897b8ae9
Bryce Berger <bryce.z.berger@gmail.com>
2024-05-11 01:50:19 -0400

    add udev rules for FPGA cables rules

--- system/luna.nix
+++ system/luna.nix
@@ -40,6 +40,21 @@
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ATTRS{idVendor}=="1949", ATTRS{idProduct}=="0004", MODE="0664", GROUP="plugdev"
     SUBSYSTEM=="usb", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="6146", MODE="0664", GROUP="plugdev"
+
+    # seme
+    ATTR{idVendor}=="0403", ATTRS{manufacturer}=="Digilent", MODE:="666"
+    ATTR{idVendor}=="0403", ATTRS{manufacturer}=="Xilinx", MODE:="666"
+    ATTR{idVendor}=="03fd", ATTR{idProduct}=="0008", MODE:="666"
+    ATTR{idVendor}=="03fd", ATTR{idProduct}=="0007", MODE:="666"
+    ATTR{idVendor}=="03fd", ATTR{idProduct}=="0009", MODE:="666"
+    ATTR{idVendor}=="03fd", ATTR{idProduct}=="000d", MODE:="666"
+    ATTR{idVendor}=="03fd", ATTR{idProduct}=="000f", MODE:="666"
+    ATTR{idVendor}=="03fd", ATTR{idProduct}=="0013", MODE:="666"
+    ATTR{idVendor}=="03fd", ATTR{idProduct}=="0015", MODE:="666"
+
+    ATTR{idVendor}=="09fb", ATTR{idProduct}=="6001", MODE:="666"
+    ATTR{idVendor}=="09fb", ATTR{idProduct}=="6010", MODE:="666"
+    ATTR{idVendor}=="09fb", ATTR{idProduct}=="6810", MODE:="666"
   '';
 
   environment.systemPackages = with pkgs; [
