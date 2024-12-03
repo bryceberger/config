a7afd5c3429c574fb52bab0d54330cd7442ffbc1
Bryce Berger <bryce.z.berger.civ@us.navy.mil>
2024-12-03 14:43:30 -0500

    refactor home/helix.nix

--- home/helix.nix
+++ home/helix.nix
@@ -6,6 +6,147 @@
   ...
 }: let
   std = nix-std.lib;
+
+  map-languages = langs:
+    builtins.map
+    (name: {inherit name;} // langs.${name})
+    (builtins.attrNames langs);
+
+  language = map-languages {
+    bash.formatter.command = "shfmt";
+    c.indent = {
+      tab-width = 4;
+      unit = "\t";
+    };
+    cpp.indent = {
+      tab-width = 4;
+      unit = "    ";
+    };
+    git-commit.file-types = [{glob = "COMMIT_EDITMSG";} "jjdescription"];
+    java.indent = {
+      tab-width = 4;
+      unit = "    ";
+    };
+    nix.formatter = {
+      command = "alejandra";
+      args = ["-q"];
+    };
+    python.language-servers = ["pyright" "ruff"];
+    toml.language-servers = ["ctags-lsp"];
+    typst.formatter.command = "typstyle";
+    verilog = {
+      language-servers = ["svls"];
+      formatter = {
+        command = "verible-verilog-format";
+        args = ["--indentation_spaces" "4" "-"];
+      };
+    };
+    veryl = {
+      file-types = ["veryl" "vl"];
+      scope = "source.veryl";
+      comment-token = "//";
+      indent = {
+        tab-width = 4;
+        unit = "    ";
+      };
+      language-servers = ["veryl-ls"];
+    };
+  };
+
+  language-server = {
+    ctags-lsp.command = "ctags-lsp";
+    nixd.command = "nixd";
+    pyright = {
+      command = "pyright-langserver";
+      args = ["--stdio"];
+    };
+    ruff = {
+      command = "ruff";
+      args = ["server" "--preview"];
+    };
+    rust-analyzer.config.check = {command = "clippy";};
+    svls.command = "svls";
+    veryl-ls = {command = "veryl-ls";};
+  };
+
+  config = {
+    theme = "catppuccin_mocha";
+
+    editor = {
+      auto-format = false;
+      bufferline = "multiple";
+      color-modes = true;
+      cursorline = true;
+      file-picker.hidden = true;
+      gutters = ["diagnostics" "diff" "line-numbers" "spacer"];
+      indent-guides.render = true;
+      line-number = "relative";
+      true-color = true;
+      cursor-shape = {
+        insert = "bar";
+        normal = "block";
+        select = "underline";
+      };
+      statusline = {
+        right = [
+          "diagnostics"
+          "version-control"
+          "position"
+          "position-percentage"
+          "file-encoding"
+        ];
+      };
+      inline-diagnostics = {
+        cursor-line = "hint";
+        other-lines = "error";
+      };
+
+      lsp.display-messages = true;
+    };
+
+    keys = {
+      normal = {
+        "$" = "goto_line_end";
+        "0" = "goto_line_start";
+        ";" = "command_mode";
+        C-h = "jump_view_left";
+        C-j = "jump_view_down";
+        C-k = "jump_view_up";
+        C-l = "jump_view_right";
+        G = "goto_file_end";
+        T = "extend_line_down";
+        X = "extend_line_up";
+        t = "extend_line_above";
+        x = "extend_line_below";
+        space = {
+          i = ":toggle lsp.display-inlay-hints";
+          o = ":format";
+          m = {
+            m = ":run-shell-command make";
+            n = ":run-shell-command ninja -C build";
+          };
+          c = {
+            r = ":run-shell-command cargo run";
+            b = ":run-shell-command cargo build";
+            t = ":run-shell-command cargo test";
+          };
+          R = ":reflow";
+        };
+      };
+      select = {
+        "$" = "goto_line_end";
+        "0" = "goto_line_start";
+        ";" = "command_mode";
+        G = "goto_file_end";
+        T = "extend_line_down";
+        X = "extend_line_up";
+        t = "extend_line_above";
+        x = "extend_line_below";
+        space.R = ":reflow";
+      };
+      insert.j.k = "normal_mode";
+    };
+  };
 in {
   home.packages = with pkgs; [
     # extra lanugage servers
@@ -23,168 +164,8 @@ in {
 
   xdg.configFile = {
     "helix/languages.toml".text = std.serde.toTOML {
-      language = [
-        {
-          name = "bash";
-          formatter.command = "shfmt";
-        }
-        {
-          name = "python";
-          language-servers = ["pyright" "ruff"];
-        }
-        {
-          name = "cpp";
-          indent = {
-            tab-width = 4;
-            unit = "    ";
-          };
-        }
-        {
-          name = "c";
-          indent = {
-            tab-width = 4;
-            unit = "\t";
-          };
-        }
-        {
-          name = "java";
-          indent = {
-            tab-width = 4;
-            unit = "    ";
-          };
-        }
-        {
-          name = "verilog";
-          language-servers = ["svls"];
-          formatter = {
-            command = "verible-verilog-format";
-            args = ["--indentation_spaces" "4" "-"];
-          };
-        }
-        {
-          name = "nix";
-          formatter = {
-            command = "alejandra";
-            args = ["-q"];
-          };
-        }
-        {
-          name = "veryl";
-          file-types = ["veryl" "vl"];
-          scope = "source.veryl";
-          comment-token = "//";
-          indent = {
-            tab-width = 4;
-            unit = "    ";
-          };
-          language-servers = ["veryl-ls"];
-        }
-        {
-          name = "git-commit";
-          file-types = [{glob = "COMMIT_EDITMSG";} "jjdescription"];
-        }
-        {
-          name = "typst";
-          formatter = {
-            command = "typstyle";
-          };
-        }
-      ];
-
-      language-server = {
-        svls.command = "svls";
-        rust-analyzer.config.check = {
-          command = "clippy";
-        };
-        veryl-ls = {
-          command = "veryl-ls";
-        };
-        nixd.command = "nixd";
-        pyright = {
-          command = "pyright-langserver";
-          args = ["--stdio"];
-        };
-        ruff = {
-          command = "ruff";
-          args = ["server" "--preview"];
-        };
-      };
+      inherit language language-server;
     };
-    "helix/config.toml".text = std.serde.toTOML {
-      theme = "catppuccin_mocha";
-
-      editor = {
-        auto-format = false;
-        bufferline = "multiple";
-        color-modes = true;
-        cursorline = true;
-        file-picker.hidden = true;
-        gutters = ["diagnostics" "diff" "line-numbers" "spacer"];
-        indent-guides.render = true;
-        line-number = "relative";
-        true-color = true;
-        cursor-shape = {
-          insert = "bar";
-          normal = "block";
-          select = "underline";
-        };
-        statusline = {
-          right = [
-            "diagnostics"
-            "version-control"
-            "position"
-            "position-percentage"
-            "file-encoding"
-          ];
-        };
-        inline-diagnostics = {
-          cursor-line = "hint";
-          other-lines = "error";
-        };
-      };
-
-      keys = {
-        normal = {
-          "$" = "goto_line_end";
-          "0" = "goto_line_start";
-          ";" = "command_mode";
-          C-h = "jump_view_left";
-          C-j = "jump_view_down";
-          C-k = "jump_view_up";
-          C-l = "jump_view_right";
-          G = "goto_file_end";
-          T = "extend_line_down";
-          X = "extend_line_up";
-          t = "extend_line_above";
-          x = "extend_line_below";
-          space = {
-            i = ":toggle lsp.display-inlay-hints";
-            o = ":format";
-            m = {
-              m = ":run-shell-command make";
-              n = ":run-shell-command ninja -C build";
-            };
-            c = {
-              r = ":run-shell-command cargo run";
-              b = ":run-shell-command cargo build";
-              t = ":run-shell-command cargo test";
-            };
-            R = ":reflow";
-          };
-        };
-        select = {
-          "$" = "goto_line_end";
-          "0" = "goto_line_start";
-          ";" = "command_mode";
-          G = "goto_file_end";
-          T = "extend_line_down";
-          X = "extend_line_up";
-          t = "extend_line_above";
-          x = "extend_line_below";
-          space.R = ":reflow";
-        };
-        insert.j.k = "normal_mode";
-      };
-    };
+    "helix/config.toml".text = std.serde.toTOML config;
   };
 }
