d8a390771b547ef520511f34bbdc9cc9326a7fa3
Bryce Berger <bryce.z.berger@gmail.com>
2025-01-01 04:28:01 -0500

    rocm build failure

--- home/utils.nix
+++ home/utils.nix
@@ -2,10 +2,12 @@
   home.packages = with pkgs; [
     du-dust
     bat
-    (btop.override {rocmSupport = true;})
-    (pkgs.writeShellScriptBin "btop-no-gpu" ''
-      exec -a $0 ${btop}/bin/btop "$@"
-    '')
+    # https://github.com/NixOS/nixpkgs/issues/368672
+    # (btop.override {rocmSupport = true;})
+    # (pkgs.writeShellScriptBin "btop-no-gpu" ''
+    #   exec -a $0 ${btop}/bin/btop "$@"
+    # '')
+    btop
     killall
     jq
   ];
