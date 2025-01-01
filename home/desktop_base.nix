6bffb89906c9e42adeab77b82f49394f4b8aee65
Bryce Berger <bryce.z.berger@gmail.com>
2025-01-01 04:44:59 -0500

    refactor desktop_base

--- home/desktop_base.nix
+++ home/desktop_base.nix
@@ -6,44 +6,45 @@
   inherit (builtins) filter attrValues;
   inherit (pkgs.lib) isDerivation;
 
+  # something something infinite recursion if these are imported from pkgs.lib
+  option = default: cond: val:
+    if cond
+    then val
+    else default;
+  optionals = option [];
+  optionalAttrs = option {};
+
   is-not-mimas = hostname != "mimas";
 
-  always-packages = with pkgs;
-    [
-      fira-code
-      font-awesome
-      dejavu_fonts
-      maple-mono
-      zathura
-      pavucontrol
-    ]
-    ++ filter isDerivation (attrValues pkgs.nerd-fonts);
+  all-nerd-fonts = filter isDerivation (attrValues pkgs.nerd-fonts);
+
+  always-packages = with pkgs; [
+    fira-code
+    font-awesome
+    maple-mono-NF
+    zathura
+    pavucontrol
+  ];
+
+  packages =
+    always-packages
+    ++ optionals is-not-mimas (with pkgs; [firefox zen-browser playerctl])
+    ++ optionals is-not-mimas all-nerd-fonts;
 in
   {
     imports = [./kitty.nix ./wezterm.nix];
     fonts.fontconfig.enable = true;
+    home.packages = packages;
   }
-  // (
-    if is-not-mimas
-    then {
-      programs.mpv = {
-        enable = true;
-        scripts = with pkgs.mpvScripts; [
-          autoload
-          sponsorblock
-          thumbfast
-          uosc
-        ];
-      };
-      home.packages = with pkgs;
-        [
-          firefox
-          zen-browser
-          playerctl
-        ]
-        ++ always-packages;
-    }
-    else {
-      home.packages = always-packages;
-    }
-  )
+  // optionalAttrs is-not-mimas
+  {
+    programs.mpv = {
+      enable = true;
+      scripts = with pkgs.mpvScripts; [
+        autoload
+        sponsorblock
+        thumbfast
+        uosc
+      ];
+    };
+  }
--- home/kitty.nix
+++ home/kitty.nix
@@ -14,9 +14,7 @@
     settings = {
       window_padding_width = 5;
       font_size = 12;
-      font_family = "Maple Mono";
-      # font_family = "FiraCode Nerd Font";
-      # font_features = "FiraCodeNF-Reg +ss08";
+      font_family = "Maple Mono NF";
     };
 
     keybindings = {
