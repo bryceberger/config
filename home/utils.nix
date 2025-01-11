e2c3628d59cb12e14adfbe52b3e8eba75b021d98
Bryce Berger <bryce.z.berger@gmail.com>
2025-01-11 18:05:25 -0500

    btop gpu support is back

--- home/utils.nix
+++ home/utils.nix
@@ -2,12 +2,10 @@
   home.packages = with pkgs; [
     du-dust
     bat
-    # https://github.com/NixOS/nixpkgs/issues/368672
-    # (btop.override {rocmSupport = true;})
-    # (pkgs.writeShellScriptBin "btop-no-gpu" ''
-    #   exec -a $0 ${btop}/bin/btop "$@"
-    # '')
-    btop
+    (btop.override {rocmSupport = true;})
+    (pkgs.writeShellScriptBin "btop-no-gpu" ''
+      exec -a $0 ${btop}/bin/btop "$@"
+    '')
     killall
     jq
   ];
