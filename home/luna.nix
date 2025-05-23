11679224b1ef66929270358dfd2c5acdcbed7f6b
Bryce Berger <bryce.z.berger@gmail.com>
2025-05-23 13:24:59 -0400

    luna: refactor base

--- home/luna.nix
+++ home/luna.nix
@@ -1,4 +1,51 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  nix_remote = pkgs.writeShellScriptBin "nix_remote" ''
+    remote=ssh-ng://janus
+    result=result
+    target=$1
+
+    store_path=`nix build --eval-store auto --store $remote --json $target | ${pkgs.jq}/bin/jq -r '.[0].outputs.out'`
+
+    nix copy --from $remote $store_path
+    ln -sfn $store_path $result
+  '';
+  kbdbacklighttoggle = pkgs.writeShellScriptBin "kbdbacklighttoggle" ''
+    bl=${pkgs.brightnessctl}/bin/brightnessctl
+    kb=asus::kbd_backlight
+    if [ $($bl -d $kb g) -ne 0 ] ; then
+      $bl -d $kb s 0
+    else
+      $bl -d $kb s 1
+    fi
+  '';
+
+  pw = pkgs.writeScriptBin "pw" ''
+    #!${pkgs.fish}/bin/fish
+
+    if test -z $fmt
+        set fmt "+%H:%M:%S"
+    end
+
+    while true
+        set out (p)
+        if test "$out" != "$prev"
+            date $fmt | tr -d "\n"
+
+            set split (string split " " $out)
+
+            printf " %s %5.2f\n" $split[1] $split[2]
+            set prev $out
+        end
+        sleep 1
+    end
+  '';
+
+  p = pkgs.writeScriptBin "p" ''
+    #!${pkgs.fish}/bin/fish
+    set base "/sys/class/power_supply/BAT0"
+    printf "%s %2.2f" (cat $base/status | head -c 1) (math (cat $base/power_now) / 1000000)
+  '';
+in {
   imports = [
     ./common.nix
     ./devel.nix
@@ -28,50 +75,10 @@
   home.stateVersion = "23.05"; # Please read the comment before changing.
 
   home.packages = with pkgs; [
-    (writeShellScriptBin "nix_remote" ''
-      remote=ssh-ng://janus
-      result=result
-      target=$1
-
-      store_path=`nix build --eval-store auto --store $remote --json $target | ${jq}/bin/jq -r '.[0].outputs.out'`
-
-      nix copy --from $remote $store_path
-      ln -sfn $store_path $result
-    '')
-    (writeShellScriptBin "kbdbacklighttoggle" ''
-      bl=${pkgs.brightnessctl}/bin/brightnessctl
-      kb=asus::kbd_backlight
-      if [ $($bl -d $kb g) -ne 0 ] ; then
-        $bl -d $kb s 0
-      else
-        $bl -d $kb s 1
-      fi
-    '')
-    (writeScriptBin "pw" ''
-      #!${fish}/bin/fish
-
-      if test -z $fmt
-          set fmt "+%H:%M:%S"
-      end
-
-      while true
-          set out (p)
-          if test "$out" != "$prev"
-              date $fmt | tr -d "\n"
-
-              set split (string split " " $out)
-
-              printf " %s %5.2f\n" $split[1] $split[2]
-              set prev $out
-          end
-          sleep 1
-      end
-    '')
-    (writeScriptBin "p" ''
-      #!${fish}/bin/fish
-      set base "/sys/class/power_supply/BAT0"
-      printf "%s %2.2f" (cat $base/status | head -c 1) (math (cat $base/power_now) / 1000000)
-    '')
+    nix_remote
+    kbdbacklighttoggle
+    pw
+    p
     sway-audio-idle-inhibit
   ];
 
