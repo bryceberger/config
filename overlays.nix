9c0f49b29a046243304a0f2671ccddfbde5d759c
Bryce Berger <bryce.z.berger@gmail.com>
2025-06-05 15:33:34 -0400

    use proper overrideAttrs for starship
    the reason I didn't do this before was just because I couldn't figure
    out how to override the `cargoHash` properly.

--- overlays.nix
+++ overlays.nix
@@ -2,37 +2,46 @@
   system,
   inputs,
 }: final: prev: let
-  make-starship = prev: args: let
-    src = prev.fetchFromGitHub {
-      owner = "bryceberger";
-      repo = "starship";
-      inherit (args) rev hash;
-    };
-  in
-    prev.callPackage ./pkgs/starship.nix ({
+  make-starship = {
+    name ? "starship",
+    rev,
+    hash,
+    cargoHash,
+  }:
+    prev.starship.overrideAttrs rec {
+      pname = name;
+      src = prev.fetchFromGitHub {
+        owner = "bryceberger";
+        repo = "starship";
+        inherit rev hash;
+      };
+      cargoDeps = prev.rustPlatform.fetchCargoVendor {
         inherit src;
-        pname = "starship-${args.name}";
-        version = "1.22.1";
-      }
-      // args);
+        hash = cargoHash;
+      };
+    };
 
-  starship-jj-shell = make-starship prev {
-    name = "jj-shell";
-    rev = "94f8cf562687a60060d6d95936d83dd99cabac6e";
-    hash = "sha256-VScMVY9nuCkrGqpH0o00LUN4FJ3SgbhJznoHoi5S66o=";
-    cargoHash = "sha256-9Dnw5fWSNrGujDfulLJe7GzUKAKVZQ7vzgMTxkeSr0o=";
+  starship-jj-shell = make-starship {
+    name = "starship-jj-shell";
+    rev = "691521ccf174064e7a234d1d6ca6027bb62146c4";
+    hash = "sha256-Iilnwpd9FGPTnHoh/UuAPEjuRCeidk4GxHvi8Zus6Ow=";
+    cargoHash = "sha256-LXqvaVGuj1x88Lrd8pg7n145i99BCC7v1Vjn98LJof4=";
   };
 
-  starship-jj-lib = make-starship prev {
-    name = "jj-lib";
-    rev = "ac39afabae459b833fd32d5ea366184f77fd8aad";
-    hash = "sha256-KsvgLjH1vgValAaPrJo3BMv4AtC4ZZPU7Tq+kVnIvNY=";
-    cargoHash = "sha256-LAUCkoNh+/+1dEG2vBrV0SCcYK4G1yP5+JzAKLgAQW8=";
-    nativeBuildInputs = [prev.pkg-config];
-    buildInputs = [prev.openssl.dev];
+  starship-jj-lib = make-starship {
+    name = "starship-jj-lib";
+    rev = "1f55e66ffdc287eceeb4b35bb3bcdfc2fc8b89ed";
+    hash = "sha256-XUJLD4Wq8v8U075X/zNkA9kVHpscHj1qxJ0jHdB8eaw=";
+    cargoHash = "sha256-7sgRv/3Ts0eHxis5ifCot5YJJOyWTlUL+/CuVXS8jy8=";
   };
 
-  difft = prev.difftastic.overrideAttrs (old: rec {
+  starship-mm = make-starship {
+    rev = "f656ead077f0909cd41a1edd6952fb7c0629a1b3";
+    hash = "sha256-KFIn3WfyeS4rFDjFaRdEDT6xlCdQ9368tNKR4PUf32M=";
+    cargoHash = "sha256-7sgRv/3Ts0eHxis5ifCot5YJJOyWTlUL+/CuVXS8jy8=";
+  };
+
+  difft = prev.difftastic.overrideAttrs rec {
     version = "0.64.0";
     src = prev.fetchFromGitHub {
       owner = "bryceberger";
@@ -45,7 +54,7 @@
       inherit src;
       hash = "sha256-QWwNU4vOA6jWVb9rHJED/VDhwMFPZCKA4Ajad3W1czw=";
     };
-  });
+  };
 
   getpackage = package: inputs.${package}.packages.${system}.default;
 in {
@@ -53,7 +62,7 @@ in {
   jj-manage = getpackage "jj-manage";
   jujutsu = getpackage "jj";
 
-  starship = starship-jj-lib;
+  starship = starship-mm;
   inherit starship-jj-lib starship-jj-shell;
 
   difftastic = difft;
