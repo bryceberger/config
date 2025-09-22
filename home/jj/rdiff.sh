35745291469be8317999f4fe5a2e944ba45c41e0
Bryce Berger <bryce.z.berger@gmail.com>
2025-09-22 11:08:50 -0400

    jj: use interdiff for rdiff

--- home/jj/rdiff.sh
+++ home/jj/rdiff.sh
@@ -10,7 +10,7 @@ target=$1
 
 # todo: configurable stream head
 # \x1f -> unit separator
-bookmark_names=$(jj log --no-graph -r 'exactly(heads(::@ & bookmarks()), 1)' -T 'local_bookmarks.map(|l| l.name() ++ "\x1f")')
+bookmark_names=$(jj log --no-graph -r "exactly(heads((trunk()..$target):: & bookmarks()), 1)" -T 'local_bookmarks.map(|l| l.name() ++ "\x1f")')
 IFS=$'\x1f' read -ra bookmarks <<<"$bookmark_names"
 [ ${#bookmarks[@]} -eq 1 ] || fail "more than one bookmark found:" "${bookmarks[@]}"
 bookmark=${bookmarks[0]}
@@ -19,4 +19,4 @@ all_refs=$(jj evolog --no-graph -T 'commit.commit_id() ++ "|"' -r "$target")
 # todo: configurable remote
 remote=$(jj log --no-graph -T 'commit_id' -n 1 -r "($all_refs none()) & ::$bookmark@origin")
 
-jj diff --from "$remote" --to "$target" "${@:2}"
+jj interdiff --from "$remote" --to "$target" "${@:2}"
