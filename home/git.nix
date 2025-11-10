9bb3b9cd1faf6e79904757e2574f201e50ad5dfa
Bryce Berger <bryce.z.berger@gmail.com>
2025-11-10 13:22:46 -0500

    update git config, difft

--- home/git.nix
+++ home/git.nix
@@ -14,27 +14,24 @@
     enable = true;
 
     # components
-    # delta.enable = true;
     lfs.enable = true;
 
-    # user
-    userEmail = email;
-    userName = "Bryce Berger";
     signing = {
       key = gpg-key;
       signByDefault = true;
     };
 
-    extraConfig = {
+    settings = {
+      user.email = email;
+      user.name = "Bryce Berger";
+
       init.defaultBranch = "main";
       merge.conflictstyle = "zdiff3";
       diff.colorMoved = "default";
       include.path = "spork";
       advice.detachedHead = false;
 
-      difftool."difft" = {
-        cmd = "${pkgs.difftastic}/bin/difft --color always $LOCAL $REMOTE | ${pkgs.delta}/bin/delta";
-      };
+      difftool."difft".cmd = "${pkgs.difftastic}/bin/difft --color always $LOCAL $REMOTE | ${pkgs.delta}/bin/delta";
     };
   };
 }
