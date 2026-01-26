0fa1530fb90270abfb0cd0f1d4a0b703123f4e50
Bryce Berger <bryce.z.berger@gmail.com>
2026-01-26 11:35:44 -0500

    dry overlays

--- overlays.nix
+++ overlays.nix
@@ -2,44 +2,42 @@
   system,
   inputs,
 }: final: prev: let
-  starship = prev.starship.overrideAttrs rec {
-    src = final.fetchFromGitHub {
-      owner = "bryceberger";
-      repo = "starship";
-      rev = "b9a8924d8cf60b6846abf9cdc7dc9bdd518c08b0";
-      hash = "sha256-UsjPTqLASxznlGB/Z95k7r0B+NbxNN/xo9bd0CC0ZyI=";
-    };
+  overrideRust = {
+    pkg,
+    owner ? "bryceberger",
+    repo ? pkg,
+    rev,
+    hash,
+    cargoHash,
+    args ? {},
+  }: let
+    src = final.fetchFromGitHub {inherit owner repo rev hash;};
     cargoDeps = final.rustPlatform.fetchCargoVendor {
       inherit src;
-      hash = "sha256-Y4DzuCEjfQqy5NhupsV+qNm4ji7SBPc79YWfvCm+Wiw=";
+      hash = cargoHash;
     };
-  };
+  in
+    prev."${pkg}".overrideAttrs (args // {inherit src cargoDeps;});
 
-  difftastic = prev.difftastic.overrideAttrs rec {
-    version = "0.68.0";
-    src = final.fetchFromGitHub {
-      owner = "bryceberger";
-      repo = "difftastic";
-      rev = "16fb3a078b0e893ca94dc4058686ff8eb43bea7d";
-      hash = "sha256-/pY/HiSjvxou2WRcbxDZbI/ASaZgD07ejGde0EQB7X0=";
-    };
-    cargoDeps = final.rustPlatform.fetchCargoVendor {
-      inherit src;
-      hash = "sha256-mEpdIeAESCRaNZ+zAuKWf/0+4npZU8l8jlQdId9tnkA=";
-    };
+  getInput = package: inputs.${package}.packages.${system}.default;
+in {
+  helix = getInput "helix";
+  jujutsu = getInput "jj";
+
+  difftastic = overrideRust {
+    pkg = "difftastic";
+    args.version = "0.68.0";
+    rev = "16fb3a078b0e893ca94dc4058686ff8eb43bea7d";
+    hash = "sha256-/pY/HiSjvxou2WRcbxDZbI/ASaZgD07ejGde0EQB7X0=";
+    cargoHash = "sha256-mEpdIeAESCRaNZ+zAuKWf/0+4npZU8l8jlQdId9tnkA=";
+  };
+  starship = overrideRust {
+    pkg = "starship";
+    rev = "b9a8924d8cf60b6846abf9cdc7dc9bdd518c08b0";
+    hash = "sha256-UsjPTqLASxznlGB/Z95k7r0B+NbxNN/xo9bd0CC0ZyI=";
+    cargoHash = "sha256-Y4DzuCEjfQqy5NhupsV+qNm4ji7SBPc79YWfvCm+Wiw=";
   };
 
   git-pkgs = final.callPackage ./overlays/git-pkgs.nix {};
   jj-manage = final.callPackage ./overlays/jj-manage.nix {};
-
-  getpackage = package: inputs.${package}.packages.${system}.default;
-in {
-  helix = getpackage "helix";
-  jujutsu = getpackage "jj";
-  inherit
-    difftastic
-    git-pkgs
-    jj-manage
-    starship
-    ;
 }
