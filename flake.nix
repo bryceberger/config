39a51b03580b7cdfce9b3536b4b4ff7e7b51e0e8
Bryce Berger <bryce.z.berger@gmail.com>
2026-09-03 12:48:55 -0400

    flake update

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     nix-hug.url = "github:eordano/nix-hug";
     nix-hug.inputs.nixpkgs.follows = "nixpkgs";
 
-    omp.url = "github:can1357/oh-my-pi/v18.0.10";
+    omp.url = "github:can1357/oh-my-pi/v18.1.6";
     omp.inputs.nixpkgs.follows = "nixpkgs";
   };
 
